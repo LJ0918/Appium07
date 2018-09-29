0339aaca1bb5a70e102321ab90e7a825a660af1b
lv <lvjinghsd@163.com>
2018-09-29 17:12:07 +0800

    first commit

---
+++ Scripts/test01.py
@@ -0,0 +1,19 @@
+import allure
+import pytest
+
+
+class TestAllure():
+    @allure.step('执行学院更新操作')
+    def test001(self):
+        allure.attach('开始断言','学院是否更新成功')
+        print('test001被执行')
+        allure.attach('断言结束','断言成功')
+
+    @allure.step('执行学院删除操作')
+    def test002(self):
+        print('test002被执行')
+
+    @pytest.allure.severity(pytest.allure.severity_level.BLOCKER)
+    def test003(self):
+        print('test003被执行')
+        assert False
